8ea34d8731237a526fd464b49920bceeaa86948a
vladimiroDEV <c.vladimiro88@gmal.com>
2017-03-04 12:37:27 +0100

    material design work

--- src/app/theme/theme.module.ts
+++ src/app/theme/theme.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router'
 import { TopNavbarComponent } from './components/top-navbar/top-navbar.component';
 import { MaterialModule } from '@angular/material';
+import 'hammerjs'
 @NgModule({
   imports: [
     CommonModule,
