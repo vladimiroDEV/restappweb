83f65c3df0a312feb5e368ce0345845acc66dc88
vladimiroDEV <c.vladimiro88@gmal.com>
2017-03-04 16:43:42 +0100

    material ok

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+ import { Http } from '@angular/http';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  spaceScreens : Array<any>;
+
+  constructor(private http:Http) {
+    this.http.get('./DataTest.json')
+    .map(response => response.json().screenshots)
+    .subscribe(res=> this.spaceScreens =  res);
+   }
 
   ngOnInit() {
+  }
+
+  likeMe (i) {
+    if(this.spaceScreens[i].liked== 0 )
+      this.spaceScreens[i].liked=1;
+      else 
+      this.spaceScreens[i].liked=0;
+  }
+
+  deleteMe(i) {
+    this.spaceScreens[i].splice(i,1);
   }
 
 }
